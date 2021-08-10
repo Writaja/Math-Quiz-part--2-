95295d2033b9d2a6a4b047ca556f6a43d420bfb1
Writaja <74670011+Writaja@users.noreply.github.com>
2021-08-10 22:46:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -81,3 +81,61 @@ else{
 }
 document.getElementById("output").innerHTML="";    
 }
+var getword=document.getElementById("word").value;
+ word=getword.toLowerCase();
+console.log(word);
+var charat1=word.charAt(1);
+console.log(charat1);
+var length_2=Math.floor(word.length/2);
+var charat2=word.charAt(length_2);
+console.log(charat2);
+var lengthminus_1=word.length-1;
+var charat3=word.charAt(lengthminus_1);
+console.log(charat3);
+var remove_charat1=word.replace(charat1,"_");
+var remove_charat2=remove_charat1.replace(charat2,"_");
+var remove_charat3=remove_charat2.replace(charat3,"_");
+console.log(remove_charat3);
+var Question_word="<h4 id='word_display'>Q. "+remove_charat3+"</h4>";
+var input_box="<br> Answer : <input type='text' id='input_box'>";
+var check_button="<br><br> <button class='btn btn-info' onclick='check()'>Check</button> ";
+var row=Question_word+input_box+check_button;
+document.getElementById("output").innerHTML=row;
+document.getElementById("word").value="";
+
+
+question_turn="number1";
+ans_turn="number2";
+function check(){
+    get_ans=document.getElementById("input_box").value;
+    ans=get_ans.toLowerCase();
+    if(ans==word){
+        if(ans_turn=="player1"){
+            player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML=player1_score;
+
+        }
+        else{
+            number1_score=number2_score+1
+            document.getElementById("number1_score").innerHTML=number2_score;
+        }    }
+        if(question_turn=="number1"){
+            question_turn="number2"
+            document.getElementById("question_number").innerHTML="question turn - "+number2_name;
+
+        } 
+        else{
+            question_turn=="player1";
+            document.getElementById("player_question").innerHTML="question turn - "+number1_name;
+        }
+        if(ans_turn=="number1"){
+            ans_turn="number2"
+            document.getElementById("player_ans").innerHTML="answer turn - "+number1_name;
+
+        }
+else{
+    ans_number="number1";
+    document.getElementById("ans-number").innerHTML="answer turn - "+number1_name;
+}
+document.getElementById("output").innerHTML="";    
+}
